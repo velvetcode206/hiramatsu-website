810a2b9ce98aa209583ea5f787b42b4253205552
velvetcode206 <77030734+velvetcode206@users.noreply.github.com>
2026-09-18 21:19:44 -0300

    feat: add i18n eslint plugin and setup rules; add i18n ally extension recommendation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
+import vueI18n from '@intlify/eslint-plugin-vue-i18n'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
@@ -13,6 +14,23 @@ export default withNuxt(
       'pnpm-lock.yaml',
     ],
     formatters: true,
+  }, {
+    files: ['**/*.vue'],
+    plugins: { '@intlify/vue-i18n': vueI18n },
+    settings: {
+      'vue-i18n': {
+        localeDir: 'i18n/locales/*.{json,json5,yaml,yml}',
+        messageSyntaxVersion: '^11.4.0',
+      },
+    },
+    rules: {
+      '@intlify/vue-i18n/no-raw-text': ['warn'],
+      '@intlify/vue-i18n/no-html-messages': ['warn'],
+      '@intlify/vue-i18n/no-v-html': ['warn'],
+      '@intlify/vue-i18n/no-deprecated-tc': ['warn'],
+      '@intlify/vue-i18n/no-missing-keys': ['warn'],
+      '@intlify/vue-i18n/no-unused-keys': ['warn'],
+    },
   }, {
     rules: {
       'node/prefer-global/process': ['off'],
